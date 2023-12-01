d62006c11fd1693f2216362a20c7b2efbdfc946e
Gaurav Date <gaurav@entelligentsia.in>
2023-12-01 13:47:39 +0530

    Episode 3 Laying the foundation

--- README.md
+++ README.md
@@ -1,9 +1,178 @@
 # React_Series - react.org
 Learn React_JS series 
 
+# Besics in reactjs from Episode-1.
+#index.html-
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>React_Series</title>
+    <link rel="stylesheet" href="./index.css">
+</head>
+
+<body>
+    <!-- using HTML  -->
+    <div id="root">
+        <h1>Hello React</h1>
+    </div>
+    <!-- ********************************************* -->
+    <!-- using javascript  -->
+    <div id="javascript">
+    </div>
+    <!-- ******************************************************** -->
+    <!-- using React -->
+    <div id="Reactser"></div>
+    <div id="heading"></div>
+    <div id="parent"></div>
+    <div id="child1"></div>
+    <div id="child2"></div>
+    <div id="supperChild"></div>
+
+    <script type="module" src="./app.js"></script>
+</body>
+
+</html>
+<!-- ************************************************************************************************************************************************************* -->
+#app.js-
+
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+// using JavaScript
+
+const helloJS = document.createElement("h1");
+helloJS.innerHTML  = "Hello world using JavaScript";
+
+const usingJS = document.getElementById("javascript");
+usingJS.appendChild(helloJS);
+<!-- //  **************************************************************************************************************************** -->
+// using React 
+
+const HelloReact = React.createElement("h1",{id:"Reactser"},"Hello World using React");
+
+const usingReact = ReactDOM.createRoot(document.getElementById("Reactser"));
+usingReact.render(HelloReact);
+
+<!-- // ******************************************************************************** -->
+
+suppose we have parent child html stucture for Eg.
+
+<!-- <div id="heading">
+  <h1></h1>
+</>div -->
 
 
 
+const heading = React.createElement("h1",{id:"heading"},"Hello World using parentReact");
+
+const rootChild = ReactDOM.createRoot(document.getElementById("heading"));
+rootChild.render(heading);
+
+<!-- // ********************************************************************************** -->
+
+suppose we have parent inside child inside write sting html stucture for Eg.
+
+<!-- <div id="parent">
+   <div id="child1">
+      <h1></h1>
+   <div>
+</>div -->
+
+
+const idChild1 = React.createElement("div",{id:"child1"},
+React.createElement("div",{id:"child1"},React.createElement("h1",{},"Hello World using child1-React")));
+
+const childroot = ReactDOM.createRoot(document.getElementById("child1"));
+childroot.render(idChild1);
+
+<!-- // ************************************************************************************* -->
+
+
+suppose we have parent inside child1 and child2 inside write sting html stucture for Eg.
+
+<!-- <div id="parent">
+   <div id="child1">
+      <h1></h1>
+   <div>
+    <div id="child2">
+      <h1></h1>
+   <div>
+</>div -->
+
+
+
+const idchild2 = React.createElement("div",{id:"child2"},[
+    React.createElement("div",{id:"subChild1"},
+       React.createElement("h1",{},"Hello World for supperChild1")
+    ),React.createElement("div",{id:"subChild2"},
+       React.createElement("h1",{},"Hello World for supperChild2")
+  )]
+);
+
+const helloChild2 = ReactDOM.createRoot(document.getElementById("child2"));
+helloChild2.render(idchild2);
+
+<!-- // ****************************************************************************************************************************** -->
+
+
+suppose we have parent inside child1 and child2 inside write sting html with supper childs stucture for Eg.
+
+<!-- <div id="supperChild">
+   <div id="supperChild1">
+      <h1></h1>
+      <h2><h2>
+   <div>
+    <div id="child2">
+      <h1></h1>
+      <h2><h2>
+   <div>
+</>div -->
+
+
+
+const supperChild = React.createElement("div",{id:"supperChild"},[
+    React.createElement("div",{id:"supperChild1"},[
+       React.createElement("h1",{},"Hello World for supperChild1 h1"),React.createElement("h2",{},"Hello World for supperChil1 h2")]
+    ),React.createElement("div",{id:"supperChild2"},[
+       React.createElement("h1",{},"Hello World for supperChild2 h1"),React.createElement("h2",{},"Hello World for supperChild2 h2")]
+  )]
+);
+console.log(supperChild);
+const root = ReactDOM.createRoot(document.getElementById("supperChild"));
+root.render(supperChild);
+
+<!-- ********************************************************************************************************************************************* -->
+#index.css-
+
+#root{
+    color: red;
+}
+#javascript{
+    color:rgb(160, 50, 10);
+}
+#React{
+    color:crimson;
+}
+#heading{
+    color:rgb(129, 59, 96);
+}
+#parent{
+    color:rgb(200, 156, 219);
+}
+#child1{
+    color:rgb(109, 179, 109);
+}
+#child2{
+    color:rgb(133, 118, 36);
+}
+#supperChild{
+    color:rgb(145, 98, 17);
+}
+
+<!-- *********************************************************************************************************************************************************** -->
 # parcel - Parcel.com/org
 - Dev Build
 - Local Server
@@ -21,3 +190,6 @@ Learn React_JS series
 - Error Handling
 - Https mode - npx parcel index.html --https
 - Tree Shsking - remove unused code
+
+# JSX-
+# Babel-  babeljs.io
--- app.js
+++ app.js
@@ -1,105 +1,112 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-// using JavaScript
+const heading = React.createElement("h1", { id: "root" }, "Hello React");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
 
-const helloJS = document.createElement("h1");
-helloJS.innerHTML  = "Hello world using JavaScript";
+// using JSX (trancepiled code before its reaches the js) - Parcel Babel
+// JSX => Babel trancepiled it to React.createElement => React.createElement- JS object{} => HTML element (render)
 
-const usingJS = document.getElementById("javascript");
-usingJS.appendChild(helloJS);
-//  ****************************************************************************************************************************
-// using React 
+// for single line code
+// const jsxHeading = <h1 id="root" className="head">Hello react using JSX</h1>
 
-const HelloReact = React.createElement("h1",{id:"Reactser"},"Hello World using React");
-
-const usingReact = ReactDOM.createRoot(document.getElementById("Reactser"));
-usingReact.render(HelloReact);
-
-// ********************************************************************************
-/*
-suppose we have parent child html stucture for Eg.
-
-<div id="heading">
-  <h1></h1>
-</>div
-
-*/
-
-const heading = React.createElement("h1",{id:"heading"},"Hello World using parentReact");
-
-const rootChild = ReactDOM.createRoot(document.getElementById("heading"));
-rootChild.render(heading);
-
-// **********************************************************************************
-/*
-suppose we have parent inside child inside write sting html stucture for Eg.
-
-<div id="parent">
-   <div id="child1">
-      <h1></h1>
-   <div>
-</>div
-
-*/
-const idChild1 = React.createElement("div",{id:"child1"},
-React.createElement("div",{id:"child1"},React.createElement("h1",{},"Hello World using child1-React")));
-
-const childroot = ReactDOM.createRoot(document.getElementById("child1"));
-childroot.render(idChild1);
-
-// *************************************************************************************
-
-/*
-suppose we have parent inside child1 and child2 inside write sting html stucture for Eg.
-
-<div id="parent">
-   <div id="child1">
-      <h1></h1>
-   <div>
-    <div id="child2">
-      <h1></h1>
-   <div>
-</>div
-
-*/
-
-const idchild2 = React.createElement("div",{id:"child2"},[
-    React.createElement("div",{id:"subChild1"},
-       React.createElement("h1",{},"Hello World for supperChild1")
-    ),React.createElement("div",{id:"subChild2"},
-       React.createElement("h1",{},"Hello World for supperChild2")
-  )]
+// for multiple lines of code used ()
+// React Element:-
+const jsxHeading = (
+  <h1 id="root" className="head" tabIndex="1">
+    Hello react using JSX
+  </h1>
+);
+// or
+//react element inside react element how to write
+const jsxHeading1 = (
+  <span id="root" className="head" tabIndex="1">
+    {jsxHeading}
+    this is react element inside react element
+  </span>
 );
 
-const helloChild2 = ReactDOM.createRoot(document.getElementById("child2"));
-helloChild2.render(idchild2);
+console.log(jsxHeading);
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
+root1.render(jsxHeading);
+// **********************************************************************************************************
 
-// ******************************************************************************************************
+// React Functional Components:-
 
-/*
-suppose we have parent inside child1 and child2 inside write sting html with supper childs stucture for Eg.
+const HeadingComponent1 = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1 className="heading"> a function which returns the JSX element</h1>
+    </div>
+  );
+};
+const root2 = ReactDOM.createRoot(document.getElementById("root"));
+root2.render(<HeadingComponent1 />);
 
-<div id="supperChild">
-   <div id="supperChild1">
-      <h1></h1>
-      <h2><h2>
-   <div>
-    <div id="child2">
-      <h1></h1>
-      <h2><h2>
-   <div>
-</>div
+// or
 
-*/
-
-const supperChild = React.createElement("div",{id:"supperChild"},[
-    React.createElement("div",{id:"supperChild1"},[
-       React.createElement("h1",{},"Hello World for supperChild1 h1"),React.createElement("h2",{},"Hello World for supperChil1 h2")]
-    ),React.createElement("div",{id:"supperChild2"},[
-       React.createElement("h1",{},"Hello World for supperChild2 h1"),React.createElement("h2",{},"Hello World for supperChild2 h2")]
-  )]
+const HeadingComponent2 = () => (
+  <h1>
+    both example doing same when we write without using return it should be in
+    single line
+  </h1>
 );
-console.log(supperChild);
-const root = ReactDOM.createRoot(document.getElementById("supperChild"));
-root.render(supperChild);
+const root3 = ReactDOM.createRoot(document.getElementById("root"));
+root3.render(<HeadingComponent2 />);
+
+// ******************************************************************************************************************
+// Component composition:- one component inside another component
+// using arrow function
+const Title = () => <h1 className="comp">Hellow React</h1>;
+
+const HeadingComponent3 = () => {
+  return (
+    <div id="container">
+      <Title />
+      {Title()}
+      <Title></Title>
+      <h1 className="heading"> a function which returns the JSX element</h1>
+    </div>
+  );
+};
+const root4 = ReactDOM.createRoot(document.getElementById("root"));
+root4.render(<HeadingComponent3 />);
+
+//using normal function
+
+const HeadingComponent4 = function () {
+  return (
+    <div id="hello" className="normal" tabIndex="1">
+      <h2 className="compo">component composition using normal function</h2>
+    </div>
+  );
+};
+const root5 = ReactDOM.createRoot(document.getElementById("root"));
+root5.render(<HeadingComponent4 />);
+
+//  **********************************************************************************************************************
+
+// when we used {} curly braces inside jsx code you have to writen any piece of code inside this curly braces.
+//Eg.
+const head = "React";
+
+const HeadingComponent5 = () => (
+  <div id="container">
+    {/* this is valid for render another component inside this component  */}
+    <HeadingComponent4 />
+    {/* when we used react element inside JSX how */}
+    {jsxHeading}
+    <h1 className="Jscode">{head}</h1>
+    <h2>{console.log("this is the JS code inside JSX code")}</h2>
+    <h2>this is the JSX code</h2>    
+  </div>
+);
+
+const root6 = ReactDOM.createRoot(document.getElementById("root"));
+root6.render(<HeadingComponent5 />);
+
+
+
